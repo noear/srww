16f8c8bd12625034335c6ae3ec2b1c57106476e4
noear <noear@live.cn>
2021-02-13 23:01:27 +0800

    1.0

---
+++ srww.base/src/main/java/org/noear/srww/base/XPluginImp.java
@@ -0,0 +1,15 @@
+package org.noear.srww.base;
+
+
+import org.noear.solon.SolonApp;
+import org.noear.solon.core.Plugin;
+
+/**
+ * @author noear 2021/2/13 created
+ */
+public class XPluginImp implements Plugin {
+    @Override
+    public void start(SolonApp app) {
+
+    }
+}
