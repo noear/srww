c6fbc2fdf3084ab0d8740f9f9fec4973c1516e85
noear <noear@live.cn>
2021-02-17 11:18:08 +0800

    1.0

---
+++ demo/srww-admin-demo0/src/main/java/admindemo0/App.java
@@ -0,0 +1,14 @@
+package admindemo0;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/2/17 created
+ */
+public class App {
+    public static void main(String[] args) {
+        Solon.start(App.class, args, app -> {
+            app.cfg().isWhiteMode(false);
+        });
+    }
+}
