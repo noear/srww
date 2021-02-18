75b14cb67cd5936372fac239044d4810e3f6c74a
noear <noear@live.cn>
2021-02-18 21:21:39 +0800

    1.0.1

--- demo/srww-api-demo1/src/main/java/apidemo1/controller/ApiGateway.java
+++ demo/srww-api-demo1/src/main/java/apidemo1/controller/ApiGateway.java
@@ -1,22 +1,16 @@
 package apidemo1.controller;
 
-import org.noear.snack.ONode;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
-import org.noear.solon.core.handle.Context;
-import org.noear.solon.core.handle.Gateway;
 import org.noear.srww.uapi.UapiGateway;
 import org.noear.srww.uapi.interceptor.*;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * @author noear 2021/2/10 created
  */
 @Mapping("/api/v1/app/**")
 @Controller
-public abstract class ApiGateway extends UapiGateway {
+public class ApiGateway extends UapiGateway {
     @Override
     protected void register() {
         // 快速体验：
