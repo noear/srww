cdb10ba06af9d16d62c4a83d9197e8ca69bd6571
noear <noear@live.cn>
2021-02-26 17:41:49 +0800

    1.0.3

--- demo/srww-api-demo1/src/main/java/apidemo1/dso/ErrorListener.java
+++ demo/srww-api-demo1/src/main/java/apidemo1/dso/ErrorListener.java
@@ -2,22 +2,20 @@ package apidemo1.dso;
 
 
 import apidemo1.App;
+import lombok.extern.slf4j.Slf4j;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.core.event.EventListener;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.logging.utils.TagsMDC;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * 全局未控制的异常监听
  *
  * @author noear 2021/2/17 created
  */
+@Slf4j
 @Component
 public class ErrorListener implements EventListener<Throwable> {
-    Logger logger = LoggerFactory.getLogger(App.class);
-
     @Override
     public void onEvent(Throwable err) {
         Context ctx = Context.current();
@@ -26,6 +24,6 @@ public class ErrorListener implements EventListener<Throwable> {
             TagsMDC.tag0(ctx.path());
         }
 
-        logger.error("", err);
+        log.error("{}", err);
     }
 }
--- demo/srww-api-demo2/src/main/java/apidemo2/dso/ErrorListener.java
+++ demo/srww-api-demo2/src/main/java/apidemo2/dso/ErrorListener.java
@@ -1,23 +1,19 @@
 package apidemo2.dso;
 
-
-import apidemo2.App;
+import lombok.extern.slf4j.Slf4j;
 import org.noear.solon.annotation.Component;
 import org.noear.solon.core.event.EventListener;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.logging.utils.TagsMDC;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * 全局未控制的异常监听
  *
  * @author noear 2021/2/17 created
  */
+@Slf4j
 @Component
 public class ErrorListener implements EventListener<Throwable> {
-    Logger logger = LoggerFactory.getLogger(App.class);
-
     @Override
     public void onEvent(Throwable err) {
         Context ctx = Context.current();
@@ -26,6 +22,6 @@ public class ErrorListener implements EventListener<Throwable> {
             TagsMDC.tag0(ctx.path());
         }
 
-        logger.error("", err);
+        log.error("", err);
     }
 }
