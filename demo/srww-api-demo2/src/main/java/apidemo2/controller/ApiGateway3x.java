ab23526e2e0dffcfd10009da8157d0b5ff6c4b00
noear <noear@live.cn>
2021-03-06 13:46:58 +0800

    1.0.4

--- demo/srww-api-demo2/src/main/java/apidemo2/controller/ApiGateway3x.java
+++ demo/srww-api-demo2/src/main/java/apidemo2/controller/ApiGateway3x.java
@@ -21,18 +21,19 @@ public class ApiGateway3x extends ApiGatewayBase {
         //
         // http://localhost:8080/api/v2/app/a.b.c
         //
-        before(new StartInterceptor());
+        before(new StartInterceptor()); //开始
+        before(new FuseInterceptor()); //融断
 
-        before(new ParamsParseInterceptor());
-        before(new ParamsSignCheckInterceptor(new Md5Encoder()));
-        before(new ParamsRebuildInterceptor(new AesDecoder()));
+        before(new ParamsParseInterceptor()); //参数解析
+        before(new ParamsSignCheckInterceptor(new Md5Encoder())); //参数签名较验
+        before(new ParamsRebuildInterceptor(new AesDecoder())); //参数重构
 
 
-        after(new OutputBuildInterceptor(new AesEncoder()));
-        after(new OutputSignInterceptor(new Md5Encoder()));
-        after(new OutputInterceptor());
-        after(new LogInterceptor());
-        after(new EndInterceptor("v2.api.app"));
+        after(new OutputBuildInterceptor(new AesEncoder())); //输出构建
+        after(new OutputSignInterceptor(new Md5Encoder())); //输出签名
+        after(new OutputInterceptor()); //输出
+        after(new LogInterceptor()); //日志
+        after(new EndInterceptor("v2.api.app")); //结束
 
         super.register();
     }
