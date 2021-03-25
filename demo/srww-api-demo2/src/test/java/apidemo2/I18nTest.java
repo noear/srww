4816846c57d052090fff22fb757ef0225c38783b
noear <noear@live.cn>
2021-03-25 12:13:55 +0800

    1.0.6-m13

--- demo/srww-api-demo2/src/test/java/apidemo2/I18nTest.java
+++ demo/srww-api-demo2/src/test/java/apidemo2/I18nTest.java
@@ -5,6 +5,8 @@ import org.junit.runner.RunWith;
 import org.noear.rock.utils.I18nUtils;
 import org.noear.solon.test.SolonJUnit4ClassRunner;
 import org.noear.solon.test.SolonTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author noear 2021/2/24 created
@@ -12,17 +14,24 @@ import org.noear.solon.test.SolonTest;
 @RunWith(SolonJUnit4ClassRunner.class)
 @SolonTest(App.class)
 public class I18nTest {
+    Logger logger = LoggerFactory.getLogger(LogTest1x.class);
+
     @Test
     public void test0() throws Exception {
         String note = I18nUtils.getByName("title", "");
 
-        System.out.println(note);
+        assert note != null;
+
+        logger.debug(note);
     }
 
     @Test
     public void test1() throws Exception {
         String note = I18nUtils.getByNameAndFormat("hello", "", "noear");
 
-        System.out.println(note);
+        assert note != null;
+        assert note.indexOf("noear") >= 0;
+
+        logger.debug(note);
     }
 }
