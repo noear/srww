09e8aa6d49d979ddd8bbed62cd4b0cb6f9ea3418
noear <noear@live.cn>
2021-04-22 15:18:48 +0800

    1.0.10-m4

--- demo/srww-api-demo2/src/main/java/apidemo2/controller/event/MSG_user_created.java
+++ demo/srww-api-demo2/src/main/java/apidemo2/controller/event/MSG_user_created.java
@@ -1,9 +1,11 @@
 package apidemo2.controller.event;
 
 import lombok.extern.slf4j.Slf4j;
+import org.noear.solon.cloud.CloudClient;
 import org.noear.solon.cloud.CloudEventHandler;
 import org.noear.solon.cloud.annotation.CloudEvent;
 import org.noear.solon.cloud.model.Event;
+import org.noear.water.utils.Datetime;
 
 /**
  * @author noear 2021/3/12 created
@@ -14,6 +16,15 @@ public class MSG_user_created implements CloudEventHandler {
     @Override
     public boolean handler(Event event) throws Throwable {
         log.info("有个用户创建了...");
+
+//        Datetime datetime = Datetime.Now();
+//        datetime.addDay(1);
+//
+//        CloudClient.event().publish(
+//                new Event("user.created", "test")
+//                        .tags("demo")
+//                        .scheduled(datetime.getFulltime()));
+
         return true;
     }
 }
--- srww-admin/README.md
+++ srww-admin/README.md
@@ -35,7 +35,7 @@
     <parent>
         <groupId>org.noear</groupId>
         <artifactId>srww-parent</artifactId>
-        <version>1.0.10-m3</version>
+        <version>1.0.10-m4</version>
     </parent>
     
     <dependencies>
