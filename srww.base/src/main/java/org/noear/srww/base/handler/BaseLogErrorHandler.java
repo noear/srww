ed5295b2f1af2d95712be43406e3e3cca76955e0
noear <noear@live.cn>
2021-04-28 07:14:34 +0800

    1.0.10-m18

---
+++ srww.base/src/main/java/org/noear/srww/base/handler/BaseLogErrorHandler.java
@@ -0,0 +1,27 @@
+package org.noear.srww.base.handler;
+
+import org.noear.snack.ONode;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Handler;
+import org.noear.solon.logging.utils.TagsMDC;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author noear 2021/4/9 created
+ */
+public class BaseLogErrorHandler implements Handler {
+    static Logger log = LoggerFactory.getLogger(BaseEndHandler.class);
+
+    @Override
+    public void handle(Context ctx) throws Throwable {
+        TagsMDC.tag0(ctx.path());
+
+        if (ctx.errors != null) {
+            log.error("> Header: {}\n> Param: {}\n\n< Error: {}",
+                    ONode.stringify(ctx.headerMap()),
+                    ONode.stringify(ctx.paramMap()),
+                    ctx.errors);
+        }
+    }
+}
--- srww.base/src/main/java/org/noear/srww/base/listener/BaseErrorListener.java
+++ srww.base/src/main/java/org/noear/srww/base/listener/BaseErrorListener.java
@@ -2,7 +2,6 @@ package org.noear.srww.base.listener;
 
 
 import org.noear.snack.ONode;
-import org.noear.solon.annotation.Component;
 import org.noear.solon.core.event.EventListener;
 import org.noear.solon.core.handle.Context;
 import org.noear.solon.logging.utils.TagsMDC;
@@ -14,13 +13,13 @@ import org.slf4j.LoggerFactory;
  *
  * @author noear 2021/4/21 created
  */
-public class ErrorListener implements EventListener<Throwable> {
-    static final Logger log = LoggerFactory.getLogger(ErrorListener.class);
+public class BaseErrorListener implements EventListener<Throwable> {
+    static final Logger log = LoggerFactory.getLogger(BaseErrorListener.class);
 
-    private static ErrorListener instance;
-    public static synchronized ErrorListener instance(){
+    private static BaseErrorListener instance;
+    public static synchronized BaseErrorListener instance(){
         if(instance == null){
-            instance = new ErrorListener();
+            instance = new BaseErrorListener();
         }
 
         return instance;
@@ -30,12 +29,12 @@ public class ErrorListener implements EventListener<Throwable> {
     public void onEvent(Throwable error) {
         Context ctx = Context.current();
 
-        TagsMDC.tag0("global");
+        TagsMDC.tag1("global");
 
         if (ctx == null) {
             log.error("< Error: {}", error);
         } else {
-            TagsMDC.tag1(ctx.path());
+            TagsMDC.tag0(ctx.path());
             log.error("> Header: {}\n> Param: {}\n\n< Error: {}", ONode.stringify(ctx.headerMap()), ONode.stringify(ctx.paramMap()), error);
         }
     }
