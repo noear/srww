39d674f393844588060bc73e29a2d89ea767e93c
noear <noear@live.cn>
2021-06-04 14:48:40 +0800

    1.0.16

--- srww.uapi/src/main/java/org/noear/srww/uapi/XPluginImp.java
+++ srww.uapi/src/main/java/org/noear/srww/uapi/XPluginImp.java
@@ -4,12 +4,16 @@ import org.noear.srww.uapi.validation.ValidatorFailureHandlerSrwwImp;
 import org.noear.solon.SolonApp;
 import org.noear.solon.core.Plugin;
 import org.noear.solon.extend.validation.ValidatorManager;
+import org.slf4j.MDC;
 
 public class XPluginImp implements Plugin {
     @Override
     public void start(SolonApp app) {
-        //Security.addProvider(new BouncyCastleProvider());
-
         ValidatorManager.global().onFailure(new ValidatorFailureHandlerSrwwImp());
+
+        app.filter((ctx, chain) -> {
+            chain.doFilter(ctx);
+            MDC.clear();
+        });
     }
 }
