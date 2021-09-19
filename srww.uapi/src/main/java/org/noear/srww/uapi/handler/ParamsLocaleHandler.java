842bf5507598669955c9a39526796612bc53995b
noear <noear@live.cn>
2021-09-19 20:16:10 +0800

    1.2.4

---
+++ srww.uapi/src/main/java/org/noear/srww/uapi/handler/ParamsLocaleHandler.java
@@ -0,0 +1,23 @@
+package org.noear.srww.uapi.handler;
+
+import org.noear.solon.Utils;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Handler;
+import org.noear.solon.i18n.LocaleUtil;
+import org.noear.srww.uapi.common.Attrs;
+
+/**
+ * 国际化地区设定拦截器
+ */
+public class ParamsLocaleHandler implements Handler {
+
+    @Override
+    public void handle(Context ctx) throws Throwable {
+        if (ctx.getLocale() == null) {
+            String lang = ctx.param(Attrs.g_lang);
+            if (Utils.isNotEmpty(lang)) {
+                ctx.setLocale(LocaleUtil.toLocale(lang));
+            }
+        }
+    }
+}
