c61b74f3553efe93195aacdd1885285d29efe2fb
noear <noear@live.cn>
2022-04-23 20:24:37 +0800

    1.5.3

--- _demo/srww-api-demo2/src/main/java/apidemo2/Config.java
+++ _demo/srww-api-demo2/src/main/java/apidemo2/Config.java
@@ -2,6 +2,7 @@ package apidemo2;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.noear.redisx.RedisClient;
+import org.noear.rock.solon.RockCodeI18nBundleFactory;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.cloud.CloudEventInterceptor;
@@ -9,8 +10,11 @@ import org.noear.solon.cloud.CloudJobInterceptor;
 import org.noear.solon.cloud.annotation.CloudConfig;
 import org.noear.solon.data.cache.CacheService;
 import org.noear.solon.data.cache.CacheServiceSupplier;
+import org.noear.solon.i18n.I18nBundleFactory;
 import org.noear.srww.base.interceptor.BaseEventInterceptor;
 import org.noear.srww.base.interceptor.BaseJobInterceptor;
+import org.noear.srww.uapi.app.IAppFactory;
+import org.noear.srww.uapi.app.impl.RockAppFactoryImpl;
 
 import javax.sql.DataSource;
 import java.util.Properties;
@@ -60,16 +64,16 @@ public class Config {
     /**
      * 使用 Rock 语言包工厂（替换本地语言包）
      * */
-//    @Bean
-//    public I18nBundleFactory i18nBundleFactory(){
-//        return new RockCodeI18nBundleFactory();
-//    }
+    @Bean
+    public I18nBundleFactory i18nBundleFactory(){
+        return new RockCodeI18nBundleFactory();
+    }
 
     /**
      * 使用 Rock 应用工厂（提供ak/sk）
      * */
-//    @Bean
-//    public IAppFactory iAppFactory(){
-//        return new RockAppFactoryImpl();
-//    }
+    @Bean
+    public IAppFactory iAppFactory(){
+        return new RockAppFactoryImpl();
+    }
 }
